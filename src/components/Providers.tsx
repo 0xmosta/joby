72b87caefe0e47d160dc733bf0d012f8738c541a
0xmosta <iamosta@icloud.com>
2024-10-04 20:49:01 +0200

    feat: inital setup

---
+++ src/components/Providers.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import {PrivyProvider} from '@privy-io/react-auth';
+
+export default function Providers({children}: {children: React.ReactNode}) {
+  return (
+    <PrivyProvider
+      appId="cm1v0iphw053no39vomxts3mb"
+      config={{
+        // Customize Privy's appearance in your app
+        appearance: {
+          theme: 'light',
+          accentColor: '#676FFF',
+          logo: 'https://your-logo-url',
+        },
+        // Create embedded wallets for users who don't have a wallet
+        embeddedWallets: {
+          createOnLogin: 'users-without-wallets',
+        },
+      }}
+    >
+      {children}
+    </PrivyProvider>
+  );
+}
